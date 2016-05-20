32a312aeea3563c91f86bef966061820c6d8f304
Gero Herkenrath <Gero.Herkenrath@trustedshops.de>
2016-05-20 16:27:11 +0200

    finally works...

--- Example/Tests/MyPrivateClassSpec.m
+++ Example/Tests/MyPrivateClassSpec.m
@@ -1,17 +1,17 @@
 //#import <DevPodProblem/MyPrivateClass.h>
-//#import "MyPrivateClass.h"
-//
-//
-//SpecBegin(MyPrivateClass)
-//
-//describe(@"these will pass", ^{
-//	
-//	it(@"can triple a string", ^{
-//		MyPrivateClass *test = [[MyPrivateClass alloc] init];
-//		test.privateTestString = @"B";
-//		expect([test tripleTestString]).to.equal(@"BBB");
-//	});
-//});
-//
-//SpecEnd
-//
+#import "MyPrivateClass.h"
+
+
+SpecBegin(MyPrivateClass)
+
+describe(@"these will pass", ^{
+	
+	it(@"can triple a string", ^{
+		MyPrivateClass *test = [[MyPrivateClass alloc] init];
+		test.privateTestString = @"B";
+		expect([test tripleTestString]).to.equal(@"BBB");
+	});
+});
+
+SpecEnd
+
